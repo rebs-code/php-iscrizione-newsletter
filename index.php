04008dc9ff65a5b68991ea7e2b2817a78cab919e
rebs-code <jack.rebussi@gmail.com>
2024-01-10 16:37:45 +0100

    feat: form and header in index.php

--- index.php
+++ index.php
@@ -9,6 +9,19 @@
 </head>
 
 <body>
+    <header class="container p-3">
+        <h1 class="text-center">The New Newsletter with Letters</h1>
+    </header>
+    <section class="container">
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
